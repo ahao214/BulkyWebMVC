0745a57836d9cd3b30d01921811621714903a070
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-22 14:36:15 +0800

    AddComanyToUser

--- BulkyWeb.Models/ApplicationUser.cs
+++ BulkyWeb.Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace BulkyWeb.Models
 {
@@ -7,10 +9,13 @@ namespace BulkyWeb.Models
     {
         [Required]
         public string Name { get; set; }
-
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
     }
 }
