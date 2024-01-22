b3e6e8157824b96a301027302dececa02e7fd4c8
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-22 23:29:51 +0800

    payment

--- BulkyWeb.Models/OrderHeader.cs
+++ BulkyWeb.Models/OrderHeader.cs
@@ -25,10 +25,11 @@ namespace BulkyWeb.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
-        public string PhoneNumber {  get; set; }
+        public string PhoneNumber { get; set; }
         [Required]
         public string StreetAddress { get; set; }
         [Required]
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using BulkyWeb.Models.ViewModels;
 using BulkyWeb.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Customer.Controllers
@@ -117,46 +118,46 @@ namespace BulkyWeb.Areas.Customer.Controllers
                 _unitOfWork.Save();
             }
 
-            //if (applicationUser.CompanyId.GetValueOrDefault() == 0)
-            //{
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
             //    //it is a regular customer account and we need to capture payment
             //    //stripe logic
-            //    //var domain = Request.Scheme + "://" + Request.Host.Value + "/";
-            //    //var options = new SessionCreateOptions
-            //    //{
-            //    //    SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
-            //    //    CancelUrl = domain + "customer/cart/index",
-            //    //    LineItems = new List<SessionLineItemOptions>(),
-            //    //    Mode = "payment",
-            //    //};
+                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+                var options = new SessionCreateOptions
+                {
+                    SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+                    CancelUrl = domain + "customer/cart/index",
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Mode = "payment",
+                };
 
-            //    foreach (var item in ShoppingCartVM.ShoppingCartList)
-            //    {
-            //        var sessionLineItem = new SessionLineItemOptions
-            //        {
-            //            PriceData = new SessionLineItemPriceDataOptions
-            //            {
-            //                UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
-            //                Currency = "usd",
-            //                ProductData = new SessionLineItemPriceDataProductDataOptions
-            //                {
-            //                    Name = item.Product.Title
-            //                }
-            //            },
-            //            Quantity = item.Count
-            //        };
-            //        options.LineItems.Add(sessionLineItem);
-            //    }
+                foreach (var item in ShoppingCartVM.ShoppingCartList)
+                {
+                    var sessionLineItem = new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = item.Product.Title
+                            }
+                        },
+                        Quantity = item.Count
+                    };
+                    options.LineItems.Add(sessionLineItem);
+                }
 
 
-            //    var service = new SessionService();
-            //    Session session = service.Create(options);
-            //    _unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
-            //    _unitOfWork.Save();
-            //    Response.Headers.Add("Location", session.Url);
-            //    return new StatusCodeResult(303);
+                var service = new SessionService();
+                Session session = service.Create(options);
+                _unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+                _unitOfWork.Save();
+                Response.Headers.Add("Location", session.Url);
+                return new StatusCodeResult(303);
 
-            //}
+            }
 
             return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
         }
