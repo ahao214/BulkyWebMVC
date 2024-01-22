7bee602bd9509b6e7b83121073f70ec61d917442
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-22 14:02:09 +0800

    CompanyRepository

---
+++ BulkyWeb.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.DataAccess.Data;
+using BulkyWeb.DataAccess.Repository.BaseRepository;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
