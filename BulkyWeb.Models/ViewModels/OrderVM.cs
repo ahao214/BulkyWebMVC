1494e41988aa860eb9bcbaf05189579eab06b649
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-22 23:49:31 +0800

    OrderVM

---
+++ BulkyWeb.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BulkyWeb.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
