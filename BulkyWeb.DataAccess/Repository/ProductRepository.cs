1b6c2f1b4dfc30367f86db401854219428c40dc0
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-23 17:23:20 +0800

    update images on product list

--- BulkyWeb.DataAccess/Repository/ProductRepository.cs
+++ BulkyWeb.DataAccess/Repository/ProductRepository.cs
@@ -27,6 +27,7 @@ namespace BulkyWeb.DataAccess.Repository
                 objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
                 objFromDb.Author = obj.Author;
+                objFromDb.ProductImages = obj.ProductImages;
                 
             }
         }
